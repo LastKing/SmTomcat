a1e1ef4f708bef6e60566a5e7646f70391e71ac3
李太新 <toonew@danwi.me>
2016-05-03 18:09:46 +0800

    Initial commit

---
+++ src/ex01/HttpServer.java
@@ -0,0 +1,66 @@
+package ex01;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * http 服务器
+ * Created by rain on 16-5-3.
+ */
+public class HttpServer {
+    /**
+     * WEB_ROOT is the directory where our HTML and other files reside.
+     * For this package, WEB_ROOT is the "webroot" directory under the
+     * working directory.
+     * The working directory is the location in the file system
+     * from where the java command was invoked.
+     */
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+    private boolean shutdown = false;
+
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.await();
+    }
+
+    private void await() {
+        ServerSocket serverSocket = null;
+
+        int port = 8080;
+
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                socket = serverSocket.accept();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+                Request request = new Request(inputStream);
+                request.parse();
+                Response response = new Response(outputStream);
+                response.setRequest(request);
+                response.sendStaticResource();
+
+                socket.close();
+
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
